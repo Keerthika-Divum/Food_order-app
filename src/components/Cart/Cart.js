b5b72177e89b85b43df5bdbbcf0bcb10a408d7e7
Keerthika-Divum <keerthika@divum.in>
2021-07-12 10:47:23 +0530

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,50 @@
+import { useContext } from 'react';
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+
+const Cart = (props) => {
+  const CartCtx = useContext(CartContext);
+  const totalAmount = `$${CartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = CartCtx.items.length > 0;
+  
+  const cartItemRemoveHandler = (id) => {
+    CartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    CartCtx.addItem({...item, amount: 1});
+  };
+
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {CartCtx.items.map((item) => (
+        <CartItem 
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item,id)}
+          onAdd={cartItemAddHandler.bind(null,item)}
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React ,{ Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+//import foodImage from '../../assets/food.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes.mainimage}>
+        <img src="https://media.istockphoto.com/photos/top-view-table-full-of-food-picture-id1220017909?b=1&k=6&m=1220017909&s=170667a&w=0&h=yqVHUpGRq-vldcbdMjSbaDV9j52Vq8AaGUNpYBGklXs=" alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
