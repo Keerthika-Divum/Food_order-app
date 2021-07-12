ed48456c98383b0abdab2618efa7377bf634992e
Keerthika-Divum <keerthika@divum.in>
2021-07-12 11:00:47 +0530

    jjj

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -17,7 +17,7 @@ const ModalOverlay = (props) => {
     );
 };
 
-const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById("overlays");  
 
 const Modal = (props) => {
     return (
